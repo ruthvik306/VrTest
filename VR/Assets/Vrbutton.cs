33d04a229803853fec8b8c9c5859ba8fa51ddfa0
ruthvik306 <48098314+ruthvik306@users.noreply.github.com>
2022-11-28 19:57:28 +0530

    Files upload

---
+++ VR/Assets/Vrbutton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+public class Vrbutton : MonoBehaviour
+{
+    public Image img;
+    public UnityEvent gclick;
+    public float timer = 2;
+    bool gvrstat;
+    public float gvrtimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void gvron(){
+        gvrstat = true;
+
+    }
+     public void gvroff(){
+        gvrstat = false;
+        gvrtimer=0;
+        img.fillAmount = 0;
+        
+    }
+    // Update is called once per frames
+    void Update()
+    {
+        if (gvrstat)
+        {
+            gvrtimer+=Time.deltaTime;
+            img.fillAmount =gvrtimer/timer; 
+        }        
+
+        if(gvrtimer> timer)
+        {
+            gclick.Invoke();
+        }
+    }
+}
